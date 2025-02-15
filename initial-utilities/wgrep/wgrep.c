4f3f22c02c4b201f27fc2dac660c77573a1ab66c
Herman Tan <hermanblueberry@gmail.com>
2025-02-15 23:02:08 +0800

    Finished wgrep.c

---
+++ initial-utilities/wgrep/wgrep.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void grep(FILE *fp, const char *searchterm) {
+  size_t size = 0;
+  ssize_t len = 0;
+  char *line = NULL;
+
+  while ((len = getline(&line, &size, fp)) != -1) {
+    if (strstr(line, searchterm) != NULL)
+      fprintf(stdout, "%s", line);
+  }
+
+  free(line);
+  return;
+}
+
+int main(int argc, char *argv[]) {
+  if (argc == 1) {
+    fprintf(stdout, "wgrep: searchterm [file ...]\n");
+    exit(1);
+  }
+
+  char *searchterm = argv[1];
+
+  if (argc == 2) {
+    grep(stdin, searchterm);
+    return EXIT_SUCCESS;
+  }
+
+  for (int i = 2; i < argc; i++) {
+    FILE *fp = fopen(argv[i], "r");
+
+    if (fp == NULL) {
+      fprintf(stdout, "wgrep: cannot open file\n");
+      exit(1);
+    }
+
+    grep(fp, searchterm);
+  }
+
+  return EXIT_SUCCESS;
+}
